0bed2db3a65662c901640f51830a9d97004dcb53
Harshawardhan Patil <89831742+Harshpatil7770@users.noreply.github.com>
2022-06-12 15:08:06 +0530

    Config Server and Eureka Server

---
+++ SpringCloudConfigServer/src/test/java/com/xoriant/settlement/SpringCloudConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xoriant.settlement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
